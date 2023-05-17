c7ef5ca967f748ae22171e3d02eb994ff5728f1b
JennyPersichini <nenny1096@gmail.com>
2023-05-17 15:07:41 +0200

    inizializzato database, aggiunte migration e ora spero funzioni il database

---
+++ LaMiaPizzeria/Database/PizzeriaContext.cs
@@ -0,0 +1,17 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.Database
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizze { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFListaPizze;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
