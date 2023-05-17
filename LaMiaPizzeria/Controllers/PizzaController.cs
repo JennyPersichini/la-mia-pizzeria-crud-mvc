d47a1dd85b7506fa5f6be49a056c3edcfa1a8b96
JennyPersichini <nenny1096@gmail.com>
2023-05-17 15:31:52 +0200

    Aggiunto controller

---
+++ LaMiaPizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿using LaMiaPizzeria.Database;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> ListaPizze = db.Pizze.ToList<Pizza>();
+
+                return View("Index", ListaPizze);
+            }
+
+        }
+    }
+}
