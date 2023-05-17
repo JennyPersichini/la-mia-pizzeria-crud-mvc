6c9af69654672ffb45943067ec53ddcc3ae576af
JennyPersichini <nenny1096@gmail.com>
2023-05-17 14:56:06 +0200

    Aggiunta struttura Pizza

--- LaMiaPizzeria/Models/Pizza.cs
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -1,6 +1,31 @@
-﻿namespace LaMiaPizzeria.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeria.Models
 {
     public class Pizza
     {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(300)]
+        public string Immagine { get; set; }
+
+        [MaxLength(20)]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Descrizione { get; set; }
+
+        public double Prezzo { get; set; }
+
+        public Pizza(string immagine, string nome, string descrizione, double prezzo )
+        {
+            Immagine = immagine;
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+        }
     }
 }
